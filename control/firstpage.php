4d649edd8638d3848da58e15f182174163db39dc
Hasan-MdMehedi <hasanshovon405@gmail.com>
2025-05-24 18:00:52 +0600

    updtae

---
+++ control/firstpage.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="../css/firstpage.css">
+</head>
+<body>
+    <h1>Welcome to student management system </h1>
+      <a href='adminlogin.php'>Admin login</a>
+      <a href ='adminsignup.php'> Admin signup </a>
+
+</body>
+</html>
